09f00880681fbaa80aebe08eaee06914e1c71b36
Luc Patiny <luc@patiny.com>
2022-03-11 15:59:41 +0100

    test: result of autoPeakPicking with default options

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,7 +1,7 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
-import { fromJcamp } from '..';
+import { fromJcamp, autoPeakPicking } from '..';
 
 test('fromJcamp', () => {
   const arrayBuffer = readFileSync(join(__dirname, 'data/adamantan.jdx'));
@@ -10,4 +10,8 @@ test('fromJcamp', () => {
   expect(analysis.spectra[0].variables).toHaveProperty('x');
   expect(analysis.spectra[0].variables).toHaveProperty('y');
   expect(analysis.spectra[0].variables.x.data).toHaveLength(1791);
+
+  const spectrum = analysis.getSpectrum();
+  const peaks = autoPeakPicking(spectrum);
+  expect(peaks).toHaveLength(11);
 });
